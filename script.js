78f2d9a0184f18fde08e7eef77f54cfc4549d164
ventura0121 <ventura0121@gmail.com>
2022-06-27 14:14:00 -0400

    Added additional pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+// Layout the website using HTML and CSS
+// Add necessary information to meet requiments such as bio
+// Link resume and additional contact information
+// Add a row to display projects
+// Use Javascript to create 3 points of interactions
+// Implement Responsive design
