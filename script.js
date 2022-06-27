85be991840063cbba266ef855f9d8fce371123cc
ventura0121 <ventura0121@gmail.com>
2022-06-27 14:24:54 -0400

    Got all links working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Layout the website using HTML and CSS
-// Add necessary information to meet requiments such as bio
+// Add necessary information to meet requirements such as bio
 // Link resume and additional contact information
 // Add a row to display projects
 // Use Javascript to create 3 points of interactions
 // Implement Responsive design
+
