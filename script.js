a22315e3e52ea605686e67affd00d229967aea27
ventura0121 <ventura0121@gmail.com>
2022-06-27 14:43:23 -0400

    Added bio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Layout the website using HTML and CSS
 // Add necessary information to meet requirements such as bio
-// Link resume and additional contact information
+// Link resume and additional contact information. COMPLETE
 // Add a row to display projects
 // Use Javascript to create 3 points of interactions
 // Implement Responsive design
