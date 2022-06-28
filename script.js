859706002140a594a0017bb4eb4b749fe1103b6a
ventura0121 <ventura0121@gmail.com>
2022-06-27 23:13:47 -0400

    fixed gif size

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// Layout the website using HTML and CSS
-// Add necessary information to meet requirements such as bio
+// Layout the website using HTML and CSS. COMPLETED
+// Add necessary information to meet requirements such as bio. COMPLETE
 // Link resume and additional contact information. COMPLETE
 // Add a row to display projects
 // Use Javascript to create 3 points of interactions
 // Implement Responsive design
+
+//console.log connected
+
+const gifElement = document.querySelectorAll('header')
+
+gifElement.addEventListener('mouseenter', mouseOver);
+console.log("hovered over")
+
+function mouseOver() {
+    document.getElementById("header").style.color = "red";
+    }
+
+    function mouseOut() {
+    document.getElementById("header").style.color = "red";
+    }
 
