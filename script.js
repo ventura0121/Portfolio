244db9d6ffa467e4937d1692227f0a1bafc86ff5
ventura0121 <ventura0121@gmail.com>
2022-06-28 10:24:54 -0400

    Finalized javascript

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-// Layout the website using HTML and CSS. COMPLETED
-// Add necessary information to meet requirements such as bio. COMPLETE
-// Link resume and additional contact information. COMPLETE
+// Layout the website using HTML and CSS. 
+// Add necessary information to meet requirements such as bio. 
+// Link resume and additional contact information. 
 // Add a row to display projects
 // Use Javascript to create 3 points of interactions
-// Implement Responsive design. COMPLETE
+// Implement Responsive design. 
 
 //console.log connected
 
-// const projectElement = document.querySelector(".flex-container2");
-// projectElement.addEventListener('mouseover', mouseOver);
+const projectElement = document.querySelector(".flex-container2");
 
-// function mouseOver() {
-//     projectElement.classList.toggle('blue')
-//     }
+projectElement.addEventListener('mouseenter', entering);
+function entering() {
+    projectElement.classList.toggle('blue')
+    }
 
+const gifElement = document.querySelector('img');
 
+gifElement.addEventListener('click', expandGif);
+function expandGif() {
+    gifElement.classList.toggle('imgResize');
+    }
 
-// const gifElement = document.querySelector('img');
-// gifElement.addEventListener('mouseover', expandGif);
-
-// function expandGif() {
-//     gifElement.classList.toggle('size');
-//     }
 
 const informationElement = document.querySelector('.projects');
 const informationLinks = document.querySelector('.flex-container2');
 
 informationElement.addEventListener('click', hideFont);
-
 function hideFont() {
-    informationLinks.classList.toggle('hide');;
+    informationLinks.classList.toggle('hide');
     }
 
